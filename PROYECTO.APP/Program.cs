ab23edec5949b172f432251db42f4753fd0ae1db
Bustos Guerrero Juan Eduardo <eduareyesdo567@gmail.com>
2024-02-21 19:12:24 -0600

    Basico

---
+++ PROYECTO.APP/Program.cs
@@ -0,0 +1,26 @@
+﻿using PROYECTO.CORE.Entities;
+using PROYECTO.CORE.Managers;
+using PROYECTO.CORE.Services; 
+
+namespace PROYECTO.APP;
+
+public static class Program
+{
+    public static void Main(String[]args)
+    {
+        float dinero = 0;
+        
+        System.Console.Write("Ingresa dinero: ");
+        Single.TryParse(System.Console.ReadLine(), out dinero);
+
+        var salary = new Income{money = dinero};
+        var service = new FinService();
+        var managers = new FinManager(service);
+
+        //Core.Entites.ISR isr = managers.Getisr(salary);
+        Finance fin = managers.GetFinance(salary);
+        //System.Console.WriteLine($"BMI {bmi.Index} Type {bmi.BmiType}");
+        System.Console.WriteLine($"ISR: {fin.Index}");
+    }
+}
+
